70806cfb4e4e049a37f16c9d1f688e245657d757
SmolnikNata <145448920+SmolnikNata@users.noreply.github.com>
2023-11-15 21:13:09 +0100

    Add files via upload

---
+++ Homework5/script/script.js
@@ -0,0 +1,119 @@
+1
+let d = 7;
+console.log(d);
+console.log(d*d);
+console.log(d**3);
+
+2
+
+for(let i =1; i<=3; i++ ){
+    if(i==1){
+        console.log(d);   
+    }
+    else if (i==2){
+        console.log(d*d);
+    }
+    else {
+        console.log(d**3);
+    }
+}
+
+//2
+// let num1 = prompt("Введите первое число");
+// console.log(num1);
+// let num2 = prompt("Введите второе число");
+// console.log(num2);
+// if(num1>num2){
+//     console.log(Number(num1)+Number(num2));
+// }else if(num1<num2){
+//     console.log(num1*num2);
+// }else{
+//     console.log("They are identical")
+// }
+
+//3
+// let a = prompt("Введите число:");
+// if (a> 0) {
+//     let b = a ** 3;
+//     console.log(b);
+// }
+
+//4
+// let students = prompt("Students");
+// console.log(students);
+
+// let chair = prompt("Chair");
+// console.log(chair);
+
+// if(students<= chair){
+//     console.log("there are enought chairs")
+// }else{
+//     console.log("there are not enought chairs") 
+// }
+
+//5
+// for(let num =1; num<10; num++){
+//    console.log(num);
+// }
+
+ //6
+//  let arr = [2,5,18,24,2,7,16,78,15,-7];
+//  for(let i=0; i < arr.length; i++){
+//     if( arr[i]>=15){
+//         console.log(arr[i]);
+//     }
+//  }
+
+//7
+// let arr = [2,5,18,24,2,7,16,78,15,-7];
+// for(let i=0; i < arr.length; i++){
+//     if(arr[i] % 2 !== 0)
+//     console.log(arr[i]);
+//     }
+
+//8
+// let arr = [2,5,18,24,2,7,16,78,15,-7];
+// for(let i = 0; i <arr.length;i+=3){
+//     console.log(i);
+// }
+
+//9
+// let arr = [2,5,18,24,2,7,16,78,15,-7];
+// let sum = 0;
+// for(let i =0; i< arr.length; i++){
+//     if(arr[i] % 2!==0){
+//          sum += arr[i];      
+//     }  
+// }
+// console.log(sum);
+
+//10
+// let arr = [2,5,18,24,2,7,16,78,15,-7];
+// let sumEven = 0;
+// let sumNotEven=0;
+
+// for(let i =0; i< arr.length; i++){
+//        if(arr[i] % 2==0){
+//        sumEven += arr[i];
+//        }
+//        else if(arr[i] % 2!==0){
+//         sumNotEven += arr[i];
+//        }
+// }   
+//           console.log(sumEven);
+//           console.log(sumNotEven);
+
+//          if(sumEven>sumNotEven){
+//             console.log(sumEven - sumNotEven); 
+//         }else{
+//             console.log(sumNotEven - sumEven);
+//         } 
+          
+       
+
+
+  
+
+
+
+
